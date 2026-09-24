5d1315b2f9ac39eb6d88f79714787f1f0d1d970f
PineNotApp1e <nathanael.grd@eduge.ch>
2026-09-24 08:28:54 +0200

    Update SQL dump with new database structure

--- horaire-eleve/sql/init.sql
+++ horaire-eleve/sql/init.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Base de données : `horaire`
 --
-
+CREATE DATABASE IF NOT EXISTS horaire;
+USE horaire;
 -- --------------------------------------------------------
 
 --
